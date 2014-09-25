1f1f7b4d3941019bbdbc340cf98930caef77306a
paul de wouters <paul@hmn.md>
2014-09-24 21:38:57 -0300

    Refactor

--- inc/class-badass-registration.php
+++ inc/class-badass-registration.php
@@ -20,7 +20,11 @@ class Badass_Registration {
 	private static $_instance;
 
 	/**
+	 * Builds the Registration object.
 	 *
+	 * @param array $fields
+	 *
+	 * @throws \Exception
 	 */
 	private function __construct( array $fields ) {
 
@@ -28,9 +32,9 @@ class Badass_Registration {
 			throw new \Exception( 'No fields provided' );
 
 		add_action( 'register_form', array( $this, 'fields' ) );
-		add_action( 'user_register', array( $this, 'save_user_data' ), 10, 1 );
+		add_action( 'user_register', array( $this, 'save_user_data' ) );
 
-		add_filter( 'shake_error_codes', array( $this, 'errors' ), 10, 2 );
+		add_filter( 'shake_error_codes', array( $this, 'errors' ) );
 		add_filter( 'registration_errors', array( $this, 'registration_errors' ), 10, 3 );
 		add_filter( 'login_messages', array( $this, 'login_messages' ) );
 
@@ -38,7 +42,7 @@ class Badass_Registration {
 
 		foreach ( $fields as $field ) {
 
-			$class = '\\BadassRegistration\\' . ucwords( $field['type'] ) . '_Field';
+			$class = '\\BadassRegistration\\' . ucwords( $field['element'] ) . '_Field';
 
 			$this->registration_fields->add(
 				new $class( $field )
@@ -49,6 +53,8 @@ class Badass_Registration {
 	}
 
 	/**
+	 * @param $fields
+	 *
 	 * @return Badass_Registration
 	 */
 	public static function get_instance( $fields ) {
@@ -66,8 +72,11 @@ class Badass_Registration {
 	public function fields() {
 
 		foreach ( $this->registration_fields as $field ) {
-			if ( isset( $_POST[ $field->id_attr ] ) ) {
-				$field->value_attr = $field->sanitize( $_POST[ $field->id_attr ] );
+
+			$id = $field->get_attribute( 'id' );
+
+			if ( isset( $_POST[ $id ] ) ) {
+				$field->set_attribute( 'value', sanitize_text_field( $_POST[ $id ] ) );
 			}
 		}
 
@@ -76,14 +85,16 @@ class Badass_Registration {
 
 	/**
 	 * @param $errors
-	 * @param $redirect_to
 	 *
 	 * @return array
 	 */
-	public function errors( $errors, $redirect_to ) {
+	public function errors( $errors ) {
 
 		foreach ( $this->registration_fields as $field ) {
-			$errors[] = 'empty_'. $field->id_attr;
+
+			$id = $field->get_attribute( 'id' );
+
+			$errors[] = 'empty_'. $id;
 		}
 
 		return $errors;
@@ -100,8 +111,12 @@ class Badass_Registration {
 	public function registration_errors( $errors, $sanitized_user_login, $user_email ) {
 
 		foreach ( $this->registration_fields as $field ) {
-			if ( 0 === strlen( trim( $_POST[ $field->id_attr ] ) ) ) {
-				$errors->add( $_POST[ $field->id_attr ], __( '<strong>ERROR</strong>: Please enter your ' . $field->label_text . '.', 'badass-registration' ), array( 'form-field' => $_POST[ $field->id_attr ] ) );
+
+			$id = $field->get_attribute( 'id' );
+			$label = $field->get_attribute( 'label' );
+
+			if ( 0 === strlen( trim( $_POST[ $id ] ) ) ) {
+				$errors->add( $_POST[ $id ], __( '<strong>ERROR</strong>: Please enter your ' . $label . '.', 'badass-registration' ), array( 'form-field' => $_POST[ $id ] ) );
 			}
 		}
 
@@ -129,8 +144,11 @@ class Badass_Registration {
 	public function save_user_data( $user_id ) {
 
 		foreach ( $this->registration_fields as $field ) {
-			if ( isset( $_POST[ $field->id_attr ] ) ) {
-				update_user_meta( $user_id, $field->id_attr, $field->sanitize( $_POST[ $field->id_attr ] ) );
+
+			$id = $field->get_attribute( 'id' );
+
+			if ( isset( $_POST[ $id ] ) ) {
+				update_user_meta( $user_id, $id, sanitize_text_field( $_POST[ $id ] ) );
 			}
 		}
 	}
